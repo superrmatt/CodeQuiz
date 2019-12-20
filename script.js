f3abcd3649204d70e844f60d76c4987d6d5e6f23
Matt Dambra <mpdambra@gmail.com>
2019-12-20 15:46:54 -0500

    initial set up of javascript functions, timer is working. trying to add to html navbar, not working as planned, will push again once complete

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
 $(document).ready(function() {
+    var seconds = 75; //total quiz length in seconds
 
-}
+    
+    buildTimer();
+
+    
+    //when an answer choice is clicked
+    $(".choices").on("click", function(){
+      
+    });
+
+
+    //run code to show next question on screen
+    function nextQuestion(){
+
+    }
+
+    //determines validity of answer
+    function isValid(){
+
+    }
+
+    //runs initial code to build the quiz, when "start quiz" button is clicked
+    function initilizeQuiz(){
+
+    }
+
+    //changes timer value, accepts argument as int in seconds, positive for positive change, negative for negative change
+    function changeTime(time){
+        seconds = seconds + time;
+        $("timer").html("Time: " + seconds);
+        console.log("changed time. seconds = " + seconds);
+    }
+
+    //builds the initial state of the timber object
+    function buildTimer(){
+        console.log("building timer");
+        console.log("seconds  = " + seconds);
+        
+        window.setInterval(start, 1000);
+        
+        //start timer
+        function start() {
+            console.log("starting timer....")
+            changeTime(-1);
+            console.log("interval changed");
+        }
+
+    }
+});
