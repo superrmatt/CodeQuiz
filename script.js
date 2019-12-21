51cec5139caa27ec2222d08c5889047816cda62e
Matt Dambra <mpdambra@gmail.com>
2019-12-20 19:00:51 -0500

    added much, almost fully funtional, quiz initializes, grabs questions and answers, working on validation of chosen answer

--- script.js
+++ script.js
@@ -19,33 +19,66 @@ $(document).ready(function() {
     var question = "";
 
     /*
-    * global variable, stores string answer choice 1
+    * global variable, stores all answers as strings
     */
-    var answerOne = "";
+    var answers = ["", "", "", ""];
 
     /*
-    * global variable, stores string answer choice 2
+    * global variable which stores correct question
     */
-    var answerTwo = "";
+    var correctAnswer = "";
 
     /*
-    * global variable, stores string answer choice 3
+    * global variable which stores question number, starts at question 0, as in, index 0 on questions.
     */
-    var answerThree = "";
-
-    /*
-    * global variable, stores string answer choice 4
-    */
-    var answerFour = "";
+    var questionNumber = 0;
 
 
     /*************************************************************************************/
     //Listeners
 
     /* 
-    * when start quiz button is clicked run initializeQuiz() function
+    * when start quiz button is clicked, initialize quiz
     */
-    $(".start-button").on("click", initializeQuiz());
+    $(".start-button").click(function(){
+         //imports the questions to head of this file
+         importQuestions();
+
+         //run getQuestion() to get first question value
+         getQuestion();
+ 
+         var intro = $("#introduction");
+ 
+         $(".start-button").remove();
+         
+         var answerGroup = $(".answer-group");
+ 
+         var answerBtnType = "button",
+             answerBtnClasses = "btn btn-primary answer-choices";
+ 
+         //builds new button template, to be added 3 times
+         var newButton = $("<button>");
+         newButton.attr("type", answerBtnType);
+         newButton.attr("class", answerBtnClasses);
+ 
+         //empty, then change attribute values to make more sense with question/answer format
+         intro.empty();
+         intro.attr("id", "question");
+ 
+         intro.html(question);
+ 
+         //adds the new buttons, since we added first button by mutation of start button, only adding 3, i = 2 for ease of readability
+         for(i = 0; i < 4; i++){
+             console.log("i = " + i);
+             newButton.html(answers[i]);
+             newButton.clone().appendTo(answerGroup);
+         }
+         
+         //build timer last so as not to penalize quiz-taker for javascript loading time if non-negligable
+         buildTimer();
+
+
+    });
 
     /* 
     * when user clicks on an answer choice
@@ -60,76 +93,44 @@ $(document).ready(function() {
     /*************************************************************************************/
     //Functions
 
-    /*
-    * initializes the quiz when start-button is clicked
-    */    
-    function initializeQuiz(){
-
-        //imports the questions to head of this file
-        importQuestions();
-
-        var intro = $("#introduction");
-        var Btn = $(".start-button");
-        
-        var answerGroup = $(".answer-group");
-
-        var answerBtnType = "button",
-            answerBtnClasses = "btn btn-primary answer-choices";
-
-        //builds new button template, to be added 3 times
-        var newButton = $("<button>");
-        newButton.attr("type", answerBtnType);
-        newButton.attr("class", answerBtnClasses);
-
-        //empty, then change attribute values to make more sense with question/answer format
-        intro.empty();
-        Btn.empty();
-        intro.attr("id", "question");
-        Btn.attr("class", answerBtnClasses);
-        Btn.html("Answer 1");
-
-        intro.html(/*Insert question here*/"???????????");
-
-        //adds the new buttons, since we added first button by mutation of start button, only adding 3, i = 2 for ease of readability
-        for(i = 2; i <= 4; i++){
-            console.log("i = " + i);
-            newButton.html("Answer " + i);
-            newButton.clone().appendTo(answerGroup);
-        }
-        
-        //run next question to get first question value
-        nextQuestion();
-        //build timer last so as not to penalize quiz-taker for javascript loading time if non-negligable
-        buildTimer();
-    }
 
     /*
     * imports questions.js
     * runs at start
     */
     function importQuestions(){
-        var imported = document.createElement('questions.js');
+        var imported = document.createElement('questions');
         imported.src = 'questions.js';
         document.head.appendChild(imported);
         return imported;
     }
 
     /*
-    * run code to show next question on screen
+    * accessor, gets current question and associated values (answers and such)
     */
-    function nextQuestion(){
-        for(i = 0; i < questions.length; i++){
-            
+    function getQuestion(){
+        //get question
+        question = questions[questionNumber].title;
+
+        //gets answers
+        for(i = 0; i < questions[questionNumber].choices.length; i++){
+            answers[i] = questions[questionNumber].choices[i];
         }
+        
+        //gets correct answer
+        correctAnswer = questions[questionNumber].answer;
     }
 
     /*
-    * determines validity of answer
+    * determines validity of chosen answer
     */
     function isValid(){
         //if correct, add time
         //if wrong, deduct time
-        //mvoe to next question
+        //increment question #
+        //run get question
+        //change html to reflect new questions
+        
 
     }
 
@@ -148,7 +149,7 @@ $(document).ready(function() {
     }
 
     /*
-    *changes timer value.  
+    * changes timer value.  
     * @arg time == int in seconds, positive for positive change, negative for negative change
     */
     function changeTime(time){
